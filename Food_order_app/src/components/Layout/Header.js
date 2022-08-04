2ebd6bfd5308cccf4067e2daa1e082dc6c3bb651
Ibad Khan <ibadk5304@gmail.com>
2022-08-04 12:05:53 -0300

    Adding modal and managing cart
    lesson 141 done

--- Food_order_app/src/components/Layout/Header.js
+++ Food_order_app/src/components/Layout/Header.js
@@ -3,11 +3,11 @@ import mealsImage from '../../assets/meals.jpeg';
 import classes from './Header.module.css';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = props => {
+const Header = (props) => {
     return <Fragment>
         <header className={classes.header}>
             <h1>React Meals</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
 
         <div className={classes['main-image']}>
